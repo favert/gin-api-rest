a98908710250f7a28c46fbd114de01db62fb215a
fabricio.vertamatti <fabricio.vertamatti@telefonica.com>
2024-12-05 22:35:06 -0300

    Added DB connection (postgres & docker)

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,10 +1,17 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/favert/api-go-gin/models"
+	"github.com/gin-gonic/gin"
+)
 
 func ExibeTodosAlunos(c *gin.Context) {
+	c.JSON(200, models.Alunos)
+}
+
+func Saudacao(c *gin.Context) {
+	nome := c.Params.ByName("nome")
 	c.JSON(200, gin.H{
-		"id":   "1",
-		"nome": "Fabricio Vertamatti",
+		"API diz: ": "E ai " + nome + ", tudo beleza?",
 	})
 }
